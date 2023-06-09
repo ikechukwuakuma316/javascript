5769064e4312c9ecdd1709ed03b61a38fbefe51f
ikechukwuakuma316 <ikechukwuakuma316@gmail.com>
2023-06-09 11:22:46 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+<!-- Course Structure
+
+Fundamentals
+Developer Skils
+DOM Manipulation 
+How JS works
+Modern Operators (ES6) - Emac Script
+Functions
+Arrays
+Numbers, Dates and timers
+Advanced DOM
+Object oriented JS
+Mapty Project
+Asynchronous JS
+Modern JS APP
+Forkify Project
+Deployment and Git 
+-->
+
+1. Code along 
+2. Coding Challenge
+3. take Notes
+
---
+++ script.js
@@ -0,0 +1,43 @@
+// let js = "amazing";
+// if (js === 'amazing');
+// // alert('JavaScript is fun!');
+// console.log(js);
+
+
+// VALUES AND VARIABLES
+// console.log("Heritage");
+// console.log(3);
+
+// let firstName = "Heritage";
+// let lastName = "Udemagwuna";
+// console.log(firstName, lastName);
+
+
+// firstName
+// first_name
+// $function
+
+// DATA TYPES
+// - Object
+// - Primitive
+
+// let me = {
+//     name: 'Heritage',
+//     age: 3,
+//     color: 'fair',
+//     handsome: true,
+//     height: '6ft'
+// };
+
+// let firstName = "Heritage";
+// let lastName = "Udemagwuna";
+
+// Number, string, boolean, undefined, Null, Symbol, BigInt
+// let age = 3;
+// let firstName = "Heritage";
+// let fullAge = true;
+// let tomorrow; 
+
+// let year;
+// console.log(year);
+// console.log(typeof year);
